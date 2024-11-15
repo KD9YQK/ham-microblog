410d32c1432f19ce1c8a881871184066c74946bc
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-15 00:57:05 -0600

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -2,33 +2,37 @@ import pyjs8call
 import db_functions
 import time
 
+numVal = {'1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E', '6': 'F', '7': 'G', '8': 'H', '9': 'I', '0': 'J'}
+abcVal = {'A': '1', 'B': '2', 'C': '3', 'D': '4', 'E': '5', 'F': '6', 'G': '7', 'H': '8', 'I': '9', 'J': '0'}
 
-numVal = {'1':'A','2':'B','3':'C','4':'D','5':'E','6':'F','7':'G','8':'H','9':'I','0':'J'}
-abcVal = {'A':'1','B':'2','C':'3','D':'4','E':'5','F':'6','G':'7','H':'8','I':'9','J':'0'}
 
-def num_to_abc(num:int):
+def num_to_abc(num: int):
     tmp = str(num)
     retval = ''
     for l in tmp:
         retval += numVal[l]
     return retval
 
-def abc_to_num(abc:str):
+
+def abc_to_num(abc: str):
     tmp = ''
     for l in abc:
         tmp += abcVal[l]
     return int(tmp)
 
-def shrink_timecode(timecode:int):
+
+def shrink_timecode(timecode: int):
     tmp = num_to_abc(timecode)
     return tmp[-5:]
 
-def expand_timecode(timecode:str):
+
+def expand_timecode(timecode: str):
     t = int(time.time())
     tmp = str(t)[:5]
     for l in timecode:
         tmp += abcVal[l]
     return int(tmp)
+
 
 # callback for new spots
 def new_spots(spots):
@@ -38,7 +42,9 @@ def new_spots(spots):
         else:
             grid = ' (' + spot.grid + ') '
 
-        print('\t--- Spot: {}{}@ {} Hz\t{}L'.format(spot.origin, grid, spot.offset, time.strftime('%x %X', time.localtime(spot.timestamp))))
+        print('\t--- Spot: {}{}@ {} Hz\t{}L'.format(spot.origin, grid, spot.offset,
+                                                    time.strftime('%x %X', time.localtime(spot.timestamp))))
+
 
 class JS8modem:
     js8call: pyjs8call.Client
@@ -48,8 +54,7 @@ class JS8modem:
         self.js8call.callback.register_command(' NEWS?', self.cb_news_cmd)
         self.js8call.callback.register_incoming(self.cb_test)
         self.js8call.callback.register_spots(new_spots)
-        
-        
+
         print("* Js8Call Modem Initialized.")
         print(f"* Host: {host} * Port: {port}")
 
@@ -59,7 +64,7 @@ class JS8modem:
 
     # Custom Callbacks
     def cb_test(self, msg):
-        print(f"{msg.origin} - {msg.text}")
+        print(f" * From: {msg.origin} To: {msg.destination} Message: {msg.text}")
 
     def cb_news_cmd(self, msg):
         # do not respond in the following cases:
@@ -76,7 +81,6 @@ class JS8modem:
             t = int(post['time'])
             message += f" {shrink_timecode(t)}"
 
-
         # respond to origin station with directed message
         self.js8call.send_directed_message(msg.origin, message)
 
@@ -86,18 +90,17 @@ if __name__ == '__main__':
         modem = JS8modem()
     except RuntimeError:
         print("ERROR - JS8Call application not installed")
-        #exit()
+        # exit()
     modem.start()
-#    ta = int(time.time())
-#    time.sleep(1)
-#    tb = int(time.time())
-#    time.sleep(1)
-#    tc = int(time.time())
+    #    ta = int(time.time())
+    #    time.sleep(1)
+    #    tb = int(time.time())
+    #    time.sleep(1)
+    #    tc = int(time.time())
 
-#    print("sending")
-#    modem.js8call.send_message(f"TEST {numVal[str(0)]} {shrink_timecode(ta)} {shrink_timecode(tb)} {shrink_timecode(tc)}")
-    
+    #    print("sending")
+    #    modem.js8call.send_message(f"TEST {numVal[str(0)]} {shrink_timecode(ta)} {shrink_timecode(tb)} {shrink_timecode(tc)}")
+
     # Main Loop
     while modem.js8call.online:
         pass
-

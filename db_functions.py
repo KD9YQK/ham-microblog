f9ed509feed10bb52a23aed518da4cdea1e60018
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-11-14 23:58:46 -0600

    Update db_functions.py

--- db_functions.py
+++ db_functions.py
@@ -2,7 +2,7 @@ import sqlite3
 import time
 
 day = 60 * 24  # Time in seconds of complete day
-
+expire = day * 3
 
 def get_time():
     return int(time.time())
@@ -10,128 +10,58 @@ def get_time():
 
 def build_db():
     con, cur = get_db()
-    # Drop the GEEK table if already exists.
-    cur.execute("DROP TABLE IF EXISTS my_blog")
-    cur.execute("DROP TABLE IF EXISTS all_blog")
-
-    # Creating table my_blog
-    table = """ CREATE TABLE my_blog (
-                time INT NOT NULL,
-                message VARCHAR(255) NOT NULL,
-                expire INT NOT NULL
-            ); """
+    cur.execute("DROP TABLE IF EXISTS blog")
 
     cur.execute(table)
-    # Creating table all_blog
-    table = """ CREATE TABLE all_blog (
+    # Creating table blog
+    table = """ CREATE TABLE blog (
                     time INT NOT NULL,
                     callsign VARCHAR(25) NOT NULL,
-                    message VARCHAR(255) NOT NULL,
-                    expire INT NOT NULL
+                    message VARCHAR(255) NOT NULL
                 ); """
 
     cur.execute(table)
     con.close()
 
 
-def get_my_blog(num: int = 3):
-    purge_expired_my_blog()
+
+def get_callsign_blog(callsign: str, num: int = 3):
+    purge_expired__blog()
     con, cur = get_db()
     if num == 0:
-        rows = cur.execute('''SELECT time, message, expire FROM my_blog ORDER BY time DESC''')
-    else:
-        rows = cur.execute('''SELECT time, message, expire FROM my_blog ORDER BY time DESC LIMIT ?''', (num,))
-    blog = []
-    for row in rows:
-        post = {
-            'time': row[0],
-            'msg': row[1],
-            'expire': row[2]
-        }
-        blog.append(post)
-    con.close()
-    return blog
-
-
-def get_my_blog(num: int = 3):
-    purge_expired_my_blog()
-    con, cur = get_db()
-    if num == 0:
-        rows = cur.execute('''SELECT time, message, expire FROM my_blog ORDER BY time DESC''')
-    else:
-        rows = cur.execute('''SELECT time, message, expire FROM my_blog ORDER BY time DESC LIMIT ?''', (num,))
-    blog = []
-    for row in rows:
-        post = {
-            'time': row[0],
-            'msg': row[1],
-            'expire': row[2]
-        }
-        blog.append(post)
-    con.close()
-    return blog
-
-
-def get_callsign_all_blog(callsign: str, num: int = 3):
-    purge_expired_all_blog()
-    con, cur = get_db()
-    if num == 0:
-        rows = cur.execute('''SELECT time, message, expire FROM all_blog WHERE callsign = ? ORDER BY time DESC''',
+        rows = cur.execute('''SELECT time, message FROM blog WHERE callsign = ? ORDER BY time DESC''',
                            (callsign,))
     else:
-        rows = cur.execute('''SELECT time, message, expire FROM all_blog WHERE callsign = ? ORDER BY time DESC LIMIT ?''',
+        rows = cur.execute('''SELECT time, message FROM blog WHERE callsign = ? ORDER BY time DESC LIMIT ?''',
                            (callsign, num,))
     blog = []
     for row in rows:
         post = {
             'time': row[0],
             'callsign': callsign,
-            'msg': row[1],
-            'expire': row[2]
+            'msg': row[1]
         }
         blog.append(post)
     con.close()
     return blog
 
 
-def add_all_blog(mtime: int, call: str, msg: str, expire: int):
-    purge_expired_all_blog()
+def add_blog(mtime: int, call: str, msg: str):
+    purge_expired_blog()
     con, cur = get_db()
-    cur.execute('''INSERT INTO all_blog (time, callsign, message, expire) VALUES (?, ?, ?, ?)''',
-                (mtime, call, msg, expire,))
+    cur.execute('''INSERT INTO blog (time, callsign, message) VALUES (?, ?, ?, ?)''',
+                (mtime, call, msg,))
     con.commit()
     con.close()
 
 
-def add_my_blog(mtime: int, msg: str, expire: int):
-    purge_expired_my_blog()
-    con, cur = get_db()
-    cur.execute('''INSERT INTO my_blog (time, message, expire) VALUES (?, ?, ?)''',
-                (mtime, msg, expire,))
-    con.commit()
-    con.close()
-
-
-def purge_expired_all_blog():
+def purge_expired_blog():
     now = get_time()
     con, cur = get_db()
-    rows = cur.execute('''SELECT time, callsign, expire FROM all_blog''')
+    rows = cur.execute('''SELECT time, callsign FROM blog''')
     for row in rows:
-        expire = day * row[2]
         if expire + row[0] < now:
-            cur.execute("DELETE FROM all_blog WHERE (callsign = ? AND time = ?)", (row[1], row[0]))
-    con.commit()
-    con.close()
-
-
-def purge_expired_my_blog():
-    now = get_time()
-    con, cur = get_db()
-    rows = cur.execute('''SELECT time, expire FROM my_blog''')
-    for row in rows:
-        expire = day * row[1]
-        if row[0] + expire < now:
-            cur.execute('''DELETE FROM my_blog WHERE 'time" = ?''', (row[0],))
+            cur.execute("DELETE FROM blog WHERE (callsign = ? AND time = ?)", (row[1], row[0]))
     con.commit()
     con.close()
 
@@ -144,7 +74,7 @@ def get_db() -> tuple[sqlite3.Connection, sqlite3.Cursor]:
 
 if __name__ == "__main__":
     build_db()
-    add_my_blog(get_time(),"My Message", 1)
-    blog = get_my_blog()
+    add_blog(get_time(),"KD9YQK","My Message")
+    blog = get_callsign_blog("KD9YQK", 0)
     print(str(blog))
 

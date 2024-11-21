48fdcb30ae9e70ebd5ffacc827fcaa84dd95dfbf
HansKillinger <hanskillinger@protonmail.com>
2024-11-21 12:36:34 -0600

    Fixed? js8modem not properly parsing messages. Removed custom callbacks. Added requesting posts from individual stations for other stations.

--- js8Modem.py
+++ js8Modem.py
@@ -1,12 +1,10 @@
-import time
-
 import pyjs8call
 import db_functions
 
 
-class cmd:  # Commands. Note the space.
-    GET_POSTS = ' POSTS?'
-    POST = ' POST'
+class Command:  # Commands. Note the space.
+    GET_POSTS = 'POST?'
+    POST = 'POST'
 
 
 def num_to_abc(num: int):  # Convert an integer to a string of Letters
@@ -35,10 +33,8 @@ class JS8modem:
 
     def __init__(self, host='127.0.0.1', port=2442):
         self.js8call = pyjs8call.Client(host=host, port=port)
-        self.js8call.callback.register_command(cmd.GET_POSTS, self.cb_get_posts)
-        self.js8call.callback.register_command(cmd.POST, self.cb_rcv_post)
-        self.js8call.callback.register_incoming(self.cb_incoming)
-        self.js8call.callback.register_spots(self.cb_new_spots)
+        self.js8call.callback.register_incoming(self._incoming_callback)
+        self.js8call.callback.register_spots(self._new_spots_callback)
 
         print("* Js8Call Modem Initialized.")
         print(f"* Host: {host} * Port: {port}")
@@ -48,14 +44,18 @@ class JS8modem:
         self.js8call.inbox.enable()
 
     ###########################################
-    # Custom Callbacks
+    # Callbacks
     ###########################################
-    def cb_incoming(self, msg):  # Test callback when any msg is received
-
-        pass
+    def _incoming_callback(self, msg):  # Test callback when any msg is received
         print(f" * From: {msg.origin} To: {msg.destination} Message: {msg.text}")
+        c = msg.text.split(' ')[0]
+        if msg.destination in ['@BLOG', self.js8call.settings.get_station_callsign()]:
+            if c == Command.GET_POSTS:
+                self._get_posts(msg)
+            elif c == Command.POST:
+                self._add_post(msg)
 
-    def cb_new_spots(self, spots):  # Callback when a new spot is received.
+    def _new_spots_callback(self, spots):  # Callback when a new spot is received.
         _t = self.is_running
         for spot in spots:
             if spot.grid in (None, ''):
@@ -66,36 +66,50 @@ class JS8modem:
             # print('\t--- Spot: {}{}@ {} Hz\t{}L'.format(spot.origin, _grid, spot.offset,
             #                                            time.strftime('%x %X', time.localtime(spot.timestamp))))
 
-    def cb_get_posts(self, msg):  # Callback when the POSTS? command is received
+    #####################################
+    # Callback Responses
+    #####################################
+    def _get_posts(self, msg):  # Callback when the POST? command is received
         # do not respond in the following cases:
         if (
                 self.js8call.settings.autoreply_confirmation_enabled() or
-                (not self.js8call.msg_is_to_me(msg) or not msg.is_directed_to('@BLOG')) or  # not directed to local station or configured group
                 msg.text in (None, '')  # message text is empty
         ):
             return
-        # collect recent posts and format to faster string format
-        blog = db_functions.get_callsign_blog(self.js8call.settings.get_station_callsign(), 1)
+        cut = msg.text.split(' ')  # [Command, ?Callsign?]
+        if len(cut) == 2:  # Callsign found, return last record from callsign
+            blog = db_functions.get_callsign_blog(cut[1], 1)
+        elif len(cut) == 1:  # Only Command, return own last record
+            blog = db_functions.get_callsign_blog(self.js8call.settings.get_station_callsign(), 1)
+        else:  # There shouldn't be more than 2. Something is wrong.
+            return
         if len(blog) < 1:
             return
-        message = f"{num_to_abc(blog[0]['time'])} {blog[0]['msg']}"
+        prefix = ''
+        if len(cut) == 2:
+            prefix = f'{blog[0]["callsign"]} '
+        message = f"{prefix}{num_to_abc(blog[0]['time'])} {blog[0]['msg']}"
 
         # respond to origin station with directed message
-        self.js8call.send_directed_command_message(msg.origin, cmd.POST, message)
-        # self.js8call.send_directed_message(msg.origin, message)
+        self.js8call.send_directed_message(msg.origin, message)
 
-    def cb_rcv_post(self, msg):
+    def _add_post(self, msg):
         _t = self.is_running
         # do not respond in the following cases:
         if msg.text in (None, ''):  # message text is empty
             return
-        t = msg.text.split('POST ')[1].split(' ')[0]
-        msg = msg.text.split('POST ')[1].split(f'{t} ')[1]
-        db_functions.add_blog(abc_to_num(t), msg.origin, msg)
+        cut = msg.text.split(' ', maxsplit=3)  # [Command, Callsign or Time, Time or Garbage]
+        if len(cut[1]) == 10:  # Timecode detected
+            msg = msg.text.split(f'{cut[1]} ')[1]
+            db_functions.add_blog(abc_to_num(cut[1]), msg.origin, msg)
+        else:  # Callsign
+            msg = msg.text.split(f'{cut[2]} ')[1]
+            db_functions.add_blog(abc_to_num(cut[2]), cut[1], msg)
 
+    #  Broadcast out a new post.
     def broadcast_post(self, post: dict, dest='@BLOG'):
-        message = f"{num_to_abc(post['time'])} {post['msg']}"
-        self.js8call.send_directed_command_message(dest, cmd.POST, message)
+        message = f"{Command.POST} {num_to_abc(post['time'])} {post['msg']}"
+        self.js8call.send_directed_message(dest, message)
 
 
 if __name__ == '__main__':

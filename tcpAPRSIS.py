d61919f1302b3e8e001df6cfc6ad97a76e650bb4
HansKillinger <hanskillinger@protonmail.com>
2024-12-12 18:18:00 -0600

    Fixed error in APRSIS pw generation

--- tcpAPRSIS.py
+++ tcpAPRSIS.py
@@ -14,9 +14,13 @@ def get_aprs_pw(callsign: str):
     cs = callsign.upper()
     i = 0
     tmp_code = 29666
+
     while i < len(cs):
         tmp_code = tmp_code ^ ord(cs[i]) * 256
-        tmp_code = tmp_code ^ ord(cs[i + 1])
+        try:
+            tmp_code = tmp_code ^ ord(cs[i + 1])
+        except IndexError:
+            pass
         i += 2
     tmp_code = tmp_code & 32767
     return tmp_code
@@ -155,8 +159,8 @@ class APRSIS:
 
 
 if __name__ == "__main__":
-    # print(get_aprs_pw('HAMBLG'))
-
+    print(get_aprs_pw('N3MEL'))
+    exit()
     async def m():
         t: APRSIS = APRSIS('HAMBLG')
         _a, _b = await t.setup(t.rx_callback)
